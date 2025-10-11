6d716bc6b47dab8e47bd020a2396419262f0a399
Trong Phan <trong.phan101204@gmail.com>
2025-10-11 20:58:05 +0700

    Adjusted logging

--- src/solve.py
+++ src/solve.py
@@ -14,13 +14,4 @@ def solve(walls, player, boxes, goals):
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
     print(f"Time taken: {elapsed_time:.3f} seconds")
-    
-    if path:
-        print("Solved! Steps:", len(path)-1)
-        
-        # Log steps
-        for s in path:
-            print("Player:", s.player)
-    else:
-        print("No solution found.")
     return path
